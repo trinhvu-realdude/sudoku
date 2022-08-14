32a977614a9ba9bd02daff1aab2dd446af3341f5
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2022-08-14 23:59:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const boardContainer = document.getElementById("board");
+const board = ONE.board;
+const answer = ONE.answer;
+
+function render() {
+    let html = "";
+
+    for (let i = 0; i < board.length; i++) {
+        html += `<div class='row' data-id='row-${i}'>`;
+
+        for (let j = 0; j < board[i].length; j++) {
+            html += `<div class='column' data-id='col-${j}'>
+                        ${board[i][j] != 0 
+                            ? board[i][j] 
+                            : `<input type='text' maxlength='1' class='input-field' id='input-${i}${j}' oninput='go(${i}, ${j})'>`} 
+                    </div>
+            `;
+        }
+
+        html += "</div>";
+    }
+
+    boardContainer.innerHTML = html;
+}
+
+render();
+
+function go(i, j) {
+    let input = parseInt(document.getElementById(`input-${i}${j}`).value);
+    board[i][j] = input;
+
+    checkAnswer();
+}
+
+function checkAnswer() {
+    const resultContainer = document.getElementById("result");
+    let compare = 0;
+    let checkFullBoard = 0;
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] != answer[i][j]) {
+                compare++;
+            }
+
+            if (board[i][j] == 0) {
+                checkFullBoard++;
+            }
+        }
+    }
+
+    if (compare == 0) {
+        resultContainer.innerHTML = "<div>Congratulations! You win</div>";
+    }
+
+    if (checkFullBoard == 0) {
+        resultContainer.innerHTML = "<div>Something wrong! Let's check again</div>";
+        setTimeout(() => {
+            $("#result").fadeOut().empty();
+        }, 3000)
+    }
+}
+
